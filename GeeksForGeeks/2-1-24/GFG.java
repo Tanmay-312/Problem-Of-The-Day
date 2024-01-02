a50575f6a4b29949427f4a7245be9012958d1d15
Tanmay-312 <jain2002tj@gmail.com>
2024-01-02 10:27:53 +0530

    Added for 2 Jan

---
+++ GeeksForGeeks/2-1-24/GFG.java
@@ -0,0 +1,23 @@
+
+        long maxi = Long.MIN_VALUE, sum = 0, prevSum = 0;
+        int j = 0;
+        
+        for(int i = 0; i < k; i++)  
+            sum += a[i];
+            
+        maxi = Math.max(maxi, sum);
+        for(int i = (int) k; i < n; i++)
+        {
+            sum += a[i];
+            prevSum += a[j++];
+            maxi = Math.max(maxi, sum);
+            
+            if(prevSum < 0) 
+            {
+                sum -= prevSum;
+                maxi = Math.max(maxi, sum);
+                prevSum = 0;
+            }
+        }
+        
+        return maxi;
---
+++ LeetCode/2-1-24/Solution.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public List<List<Integer>> findMatrix(int[] nums) 
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] count = new int[nums.length + 1];
+
+        for (final int num : nums) 
+        {
+            if (++count[num] > ans.size())
+                ans.add(new ArrayList<>());
+        
+            ans.get(count[num] - 1).add(num);
+        }
+
+        return ans;
+    }
+}
