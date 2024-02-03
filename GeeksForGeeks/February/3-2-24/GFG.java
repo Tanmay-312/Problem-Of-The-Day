2b0c41fb9c1e2c37d2008de7f10d199aa66ac8c8
Tanmay-312 <jain2002tj@gmail.com>
2024-02-03 23:59:39 +0530

    Added codes for 3 Feb

---
+++ GeeksForGeeks/February/3-2-24/GFG.java
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList1
+{
+    Node head;  // head of lisl
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node)
+    {
+        if (head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+
+            temp.next = node;
+        }
+    }
+    /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        String s = sc.nextLine();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String s1 = sc.nextLine();
+            LinkedList1 llist = new LinkedList1();
+
+            if (n > 0)
+            {
+                int a1=sc.nextInt();
+                Node head= new Node(a1);
+                llist.addToTheLast(head);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+            System.out.println(new Solution().DecimalValue(llist.head));
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}
+This is method only submission.  You only need to complete the method. */
+
+class Solution
+{
+   long DecimalValue(Node head)
+   {
+        ArrayList<Integer> list=new ArrayList<>();
+        long mod=(long)1000000007;
+        Node curr=head;
+        while(curr!=null)
+        {
+            list.add(curr.data);
+            curr=curr.next;
+        }
+        
+        Collections.reverse(list);
+        long base=1;
+        long ans=0;
+        int i=0;
+        while(i<list.size())
+        {
+            long temp=list.get(i);
+            ans +=((base%mod)*temp)%mod;
+          
+            base=((base%mod)*2)%mod;
+            i++;
+        }
+      
+        return ans%mod;
+   }
+}
