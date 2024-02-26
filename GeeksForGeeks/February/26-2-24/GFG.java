ac5e2eda607ab60b627353489c41b24735932d68
Tanmay-312 <jain2002tj@gmail.com>
2024-02-26 18:31:22 +0530

    Added codes for 26 Feb

---
+++ GeeksForGeeks/February/26-2-24/GFG.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            List<String> ans = ob.AllPossibleStrings(s);
+            for(String i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public List<String> AllPossibleStrings(String s)
+    {
+        // Code here
+        int n = s.length();
+        ArrayList<String>res=new ArrayList<>();
+        
+        for (int m = 0; m < (1 << n); m++) 
+        {
+           String sb = "";
+           for (int i = 0; i < n; i++) 
+            {
+                if ((m & (1 << i))!=0)
+                    sb += s.charAt(i);
+            }
+            
+            if (sb.length() > 0)
+                res.add(sb);
+        }
+        
+        Collections.sort(res);
+        return res;
+    }
+}
---
+++ GeeksForGeeks/February/26-2-24/README.md
@@ -0,0 +1,2 @@
+Time complexity - O(n * 2^n)
+Space complexity - O(n * 2^n)
