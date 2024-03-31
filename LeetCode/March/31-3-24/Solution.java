24434f257a336ae7496dffe70caa37a0463f5eec
Tanmay-312 <jain2002tj@gmail.com>
2024-03-31 23:35:24 +0530

    Added codes for 31 March

---
+++ LeetCode/March/31-3-24/Solution.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public long countSubarrays(int[] nums, int minK, int maxK) 
+    {
+        long ans = 0;
+        int j = -1;
+        int prevMinKIndex = -1;
+        int prevMaxKIndex = -1;
+
+        for (int i = 0; i < nums.length; ++i) 
+        {
+            if (nums[i] < minK || nums[i] > maxK)
+                j = i;
+            if (nums[i] == minK)
+                prevMinKIndex = i;
+            if (nums[i] == maxK)
+                prevMaxKIndex = i;
+            ans += Math.max(0, Math.min(prevMinKIndex, prevMaxKIndex) - j);
+        }
+
+        return ans;
+    }
+}
