d681fbaaf581ac9df94d25bc24d4cac89fba0eb5
Tanmay-312 <jain2002tj@gmail.com>
2024-04-01 18:32:15 +0530

    Added codes for 1 April

---
+++ LeetCode/April/01-4-24/Solution.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int lengthOfLastWord(String s) 
+    {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ')
+            --i;
+    
+        int lastIndex = i;
+        
+        while (i >= 0 && s.charAt(i) != ' ')
+            --i;
+
+        return lastIndex - i;
+    }
+}
