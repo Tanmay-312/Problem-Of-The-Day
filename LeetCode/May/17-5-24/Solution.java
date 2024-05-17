a76dca16f6dad7383c3ca9a3be807983540fc623
Tanmay-312 <jain2002tj@gmail.com>
2024-05-17 09:28:11 +0530

    Added codes for 17 May

---
+++ LeetCode/May/17-5-24/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public TreeNode removeLeafNodes(TreeNode root, int target) 
+    {
+        if (root == null)
+            return null;
+        
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        
+        return isLeaf(root) && root.val == target ? null : root;
+    }
+
+    private boolean isLeaf(TreeNode root) 
+    {
+        return root.left == null && root.right == null;
+    }
+}
